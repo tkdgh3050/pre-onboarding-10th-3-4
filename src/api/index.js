19413c1dcf93f723638ece36a72cd2e9257da94c
gomsang <tkdgh3050@gmail.com>
2023-05-16 01:42:00 +0900

    refactor: todo를 context API로 변경 #13

--- src/api/index.js
+++ src/api/index.js
@@ -1,21 +1,18 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_URL;
-const token = process.env.REACT_APP_TOKEN;
+function ApiRequest(baseURL, token) {
+  const baseInstance = axios.create({
+    baseURL,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  baseInstance.interceptors.response.use(({ data }) => data);
 
-const baseInstance = axios.create({
-  baseURL,
-  headers: {
-    Authorization: `Bearer ${token}`,
-  },
-});
-
-baseInstance.interceptors.response.use(({ data }) => data);
-
-const apiRequest = {
-  get: (url, request) => baseInstance.get(url, request),
-  delete: (url, request) => baseInstance.delete(url, request),
-  post: (url, data, config) => baseInstance.post(url, data, config),
-};
-
-export default apiRequest;
+  return {
+    get: (url, request) => baseInstance.get(url, request),
+    delete: (url, request) => baseInstance.delete(url, request),
+    post: (url, data, config) => baseInstance.post(url, data, config),
+  };
+}
+export default ApiRequest;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,25 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { getTodoList } from '@api/todo';
+import React from 'react';
 import Header from '@components/Header';
 import InputTodo from '@components/InputTodo';
 import TodoList from '@components/TodoList';
 
 function Main() {
-  const [todoListData, setTodoListData] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      const { data } = await getTodoList();
-      setTodoListData(data || []);
-    })();
-  }, []);
-
   return (
     <div className="container">
       <div className="inner">
         <Header />
-        <InputTodo setTodos={setTodoListData} />
-        <TodoList todos={todoListData} setTodos={setTodoListData} />
+        <InputTodo />
+        <TodoList />
       </div>
     </div>
   );
