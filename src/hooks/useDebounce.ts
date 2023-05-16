4e9f1a1ee9f17b47d5a431d49a74bb483ecf1488
gomsang <tkdgh3050@gmail.com>
2023-05-16 11:25:07 +0900

    chore: 배포를 위한 warning 제거 #24

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,5 +1,6 @@
 import { useRef } from 'react';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const useDebounce = <T extends any[]>(callback: (...args: T) => void, timeout: number) => {
   const timer = useRef<ReturnType<typeof setTimeout> | null>(null);
   return (...args: T) => {
